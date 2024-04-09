ecf79959d798ffec46d433104a6a894eaf727241
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-09 12:31:21 +0300

    Chatterbox

--- server/app.py
+++ server/app.py
@@ -14,13 +14,47 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        message_dict = [message.to_dict() for message in messages]
+        response = make_response(jsonify(message_dict), 200)
+        return response
+    
+    elif request.method == 'POST':
+        body = request.json.get('body')
+        username = request.json.get('username')
+        message = Message(body=body, username=username)
+        db.session.add(message)
+        db.session.commit()
+        
+        response = make_response(jsonify(message.to_dict()), 201)
+        return response
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+    if not message:
+            response = make_response(
+                jsonify({'error': 'Message not found'}), 404
+            )
+            return response
+    
+    if request.method == 'PATCH':
+        body = request.json.get('body')
+        message.body = body
+        db.session.commit()
+        response = make_response(
+            jsonify(message.to_dict()), 200
+        )
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        response = make_response(jsonify({}), 204)
+        return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
